f10ca296eac9d756970adaa7d7ed2233cea1c433
Starmoon30 <1973442140@qq.com>
2025-06-30 23:34:11 +0800

    星规机械体

---
+++ Source/StellarRegulator_Array/StellarRegulator_Array/CompProperties_UseEffect_ActivateMech.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using RimWorld;
+using SRA;
+using Verse;
+
+namespace SRA
+{
+    // Token: 0x02000023 RID: 35
+    public class CompProperties_UseEffect_ActivateMech : CompProperties_UseEffect
+    {
+        // Token: 0x06000079 RID: 121 RVA: 0x00003E85 File Offset: 0x00002085
+        public CompProperties_UseEffect_ActivateMech()
+        {
+            this.compClass = typeof(CompUseEffect_ActivateMech);
+        }
+
+        // Token: 0x04000037 RID: 55
+        public PawnKindDef pawnKindDef;
+
+        // Token: 0x04000038 RID: 56
+        public bool requireMechanitor = true;
+    }
+}
