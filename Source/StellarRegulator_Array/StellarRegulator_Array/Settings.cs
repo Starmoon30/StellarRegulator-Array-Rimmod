de852dca725a61cc8eec3aa9eb8c71b813e19b41
Starmoon30 <1973442140@qq.com>
2026-05-23 17:38:26 +0800

    跟进版本

--- Source/StellarRegulator_Array/StellarRegulator_Array/Settings.cs
+++ Source/StellarRegulator_Array/StellarRegulator_Array/Settings.cs
@@ -10,18 +10,12 @@ namespace SRA
     public class Setting : ModSettings
     {
         public bool
-            NoseScar = false,
-            EyeRender = true,
-            AllowDuplicateSRA_SR = false,
             AngledSRAWall = false;
 
 
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref NoseScar, "NoseScar", defaultValue: false);
-            Scribe_Values.Look(ref EyeRender, "EyeRender", defaultValue: true);
-            Scribe_Values.Look(ref AllowDuplicateSRA_SR, "AllowDuplicateSRA_SR", defaultValue: false);
             Scribe_Values.Look(ref AngledSRAWall, "AngledSRAWall", defaultValue: false);
         }
     }
@@ -29,13 +23,6 @@ namespace SRA
     public class SRAMod : Mod
     {
         public static Setting settings;
-
-        public static GameComponent_SRA_SRUtilTracker Tracker => Current.Game?.GetComponent<GameComponent_SRA_SRUtilTracker>();
-
-        public static bool NoseScar => settings.NoseScar;
-        public static bool EyeRender => ModLister.GetActiveModWithIdentifier("nals.facialanimation") == null && settings.EyeRender;
-        public static bool AllowDuplicateSRA_SR => settings.AllowDuplicateSRA_SR;
-
         public static bool AngledSRAWall => settings.AngledSRAWall;
 
         public SRAMod(ModContentPack content)
@@ -54,25 +41,7 @@ namespace SRA
             Text.Font = GameFont.Small;
             listing_Standard.GapLine();
             listing_Standard.CheckboxLabeled("SRA_AngledSRAWall_Title".Translate(), ref settings.AngledSRAWall, "SRA_AngledSRAWall_Desc".Translate());
-            listing_Standard.CheckboxLabeled("SRA_NoseScar_Title".Translate(), ref settings.NoseScar, "SRA_NoseScar_Desc".Translate());
-            if (ModLister.GetActiveModWithIdentifier("nals.facialanimation") == null)
-            {
-                listing_Standard.CheckboxLabeled("SRA_EyeRender_Title".Translate(), ref settings.EyeRender, "SRA_EyeRender_Desc".Translate());
-            }
-            listing_Standard.CheckboxLabeled("SRA_AllowDuplicateSRA_SR_Title".Translate(), ref settings.AllowDuplicateSRA_SR, "SRA_AllowDuplicateSRA_SR_Desc".Translate());
             listing_Standard.Gap();
-            if (Current.Game != null)
-            {
-                if (Tracker.SRA_SR != null)
-                {
-                    listing_Standard.Label("SRA_SRTracked".Translate(Tracker.SRA_SR.Name.ToStringFull, Tracker.SRA_SR.ThingID));
-                }
-                else
-                {
-                    listing_Standard.Label("SRA_SRNotTracked".Translate());
-
-                }
-            }
 
             listing_Standard.End();
         }
--- Source/StellarRegulator_Array/StellarRegulator_Array/ThingDefOf.cs
+++ Source/StellarRegulator_Array/StellarRegulator_Array/ThingDefOf.cs
@@ -7,7 +7,6 @@ namespace SRA
     [DefOf]
     public class SRA_DefOf
     {
-        public static PredefinedCharacterParmDef SRA_SRParm;
 
         public static PawnKindDef SRA_Mech_WarUnit_S;
 
